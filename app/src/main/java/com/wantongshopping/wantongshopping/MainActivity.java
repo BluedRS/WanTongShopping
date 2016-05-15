e05014ad9fad0b28915af238c36e90c40a938ad5
renshuai <renshuai@gmei.com>
2016-05-15 14:34:20 +0800

    增加dialog

--- app/src/main/java/com/wantongshopping/wantongshopping/MainActivity.java
+++ app/src/main/java/com/wantongshopping/wantongshopping/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wantongshopping.wantongshopping;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Dialog a = new Dialog(MainActivity.this);
 
     }
 
